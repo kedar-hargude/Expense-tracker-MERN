531be058498210905579c9b32dea5d13a6367a8c
Kedar Hargude <hargude481@gmail.com>
2022-08-23 23:52:24 +0530

    Add feature edit and delete user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,32 +1,61 @@
 const userData = require('../DUMMY_DATA');
 
 // check email and password of incoming req.body
-exports.checkEmailPasswordMiddleware = (req, res, next) => {
-    console.log(req.body);
-    if(!req.body.email || !req.body.password){
-        return res.status(404).json({status: 'error', message: 'Incomplete credentials entered'})
-    }
-    next();
-};
+// exports.checkEmailPasswordMiddleware = (req, res, next) => {
+//     console.log(req.body);
+//     if(!req.body.email || !req.body.password){
+//         return res.status(404).json({status: 'error', message: 'Incomplete credentials entered'})
+//     }
+//     next();
+// };
 
 exports.signUp = (req, res) => {
 
     // check if user email already exists
     const checkUser = userData.find(ele => ele.email === req.body.email);
     if(checkUser){
-        return res.status(409).json({ status: 'error', message: 'User email already exists. Please log in.' })
+        return res.status(409).json({ 
+            status: 'error', 
+            message: 'User email already exists. Please log in.' 
+        })
     }
 
-    res.status(201).json({status: 'success', message: `Dummy user ${req.body.name} signed up!`});
+    res.status(201).json({
+        status: 'success', 
+        message: `Dummy user ${req.body.name} signed up!`
+    });
 };
 
 exports.logIn = (req, res) => {
     const findUser = userData.find(ele => ele.email === req.body.email);
     if(!findUser){
-        res.status(404).json({ status: 'error', message: 'No user email id registered. Please sign up.'})
+        res.status(404).json({ 
+            status: 'error', 
+            message: 'No user email id registered. Please sign up.'
+        })
     } else if (findUser.password !== req.body.password){
-        res.status(404).json({ status: 'error', message: 'Wrong user password.'})
+        res.status(404).json({ 
+            status: 'error', 
+            message: 'Wrong user password.'
+        })
     } else {
-        res.status(200).json({ status: 'success', message: `Dummy user ${findUser.name} logged in!`})
+        res.status(200).json({ 
+            status: 'success', 
+            message: `Dummy user ${findUser.name} logged in!`
+        })
     }
 };
+
+exports.updateUserInfo = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet defined! Will update the user info later.'
+      });
+}
+
+exports.deleteUser = (req, res) => {
+    res.status(500).json({
+        status: 'error',
+        message: 'This route is not yet defined! Will delete the user later'
+    })
+}
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,17 @@
 const router = require('express').Router();
 const userController = require('../controllers/userController');
 
-router.use(userController.checkEmailPasswordMiddleware);
+// router.use(userController.checkEmailPasswordMiddleware);
 
 // name, email and password required
 router.post('/signup', userController.signUp);
 
 // email and password required
 router.post('/login', userController.logIn);
+
+// maybe check if the SAME user is logged in before
+router.patch('/edit', userController.updateUserInfo);
+
+router.delete('/delete', userController.deleteUser)
 
 module.exports = router;
