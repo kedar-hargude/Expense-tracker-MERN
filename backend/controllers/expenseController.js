dfb9b0102fdcbac8b254f62c7f8dd5e187065457
Kedar Hargude <hargude481@gmail.com>
2022-08-24 04:05:44 +0530

    Add expense routes and controllers

--- backend/controllers/expenseController.js
+++ backend/controllers/expenseController.js
@@ -1,1 +1,76 @@
 const userData = require('../DUMMY_DATA');
+
+exports.userIdCheck = (req, res, next) => {
+    //check if userId exists
+    const foundUser = userData.find(ele => ele._id === req.body.userId);
+    if(!req.body.userId || !foundUser){
+        return res.status(404).json({
+            status: 'error',
+            message: 'Invalid userId'
+        })
+    }
+    next();
+};
+
+exports.getAllExpenses = (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        data: userData
+    })
+};
+
+exports.checkFullRequestBody = (req, res, next) => {
+    if (!req.body.title || !req.body.amount || !req.body.recurring || !req.body.type || !req.body.date){
+        return res.status(404).json({
+            status: 'error',
+            message: 'Incomplete credentials'
+        })
+    }
+    next();
+}; 
+
+exports.checkExpenseId = (req, res, next) => {
+    if(!req.body.expenseId){
+        return res.status(404).json({
+            status: 'error',
+            message: 'Expense Id not passed'
+        })
+    }
+    next();
+};
+
+exports.addExpense = (req, res) => {
+    const foundUser = userData.find(ele => ele._id === req.body.userId);
+    // const newExpense = Object.assign({_id: nanoid()}, req.body);
+    const newExpense = {
+        _id: nanoid(),
+        title: req.body.title,
+        amount: req.body.amount,
+        recurring: req.body.recurring,
+        type: req.body.type,
+        date: req.body.date
+    }
+    foundUser.expenses.push(newExpense);
+    res.status(201).json({
+        status: 'success',
+        message: `New expense of ${req.body.title} created.`,
+        expense: newExpense
+    })
+};
+
+exports.updateExpense = (req, res) => {
+    res.status(200).json({
+        status: 'success',
+        data: {
+            expense: '<Updated expense here...>'
+        }
+    })
+};
+
+exports.deleteExpense = (req, res) => {
+    res.status(204).json({
+        status: 'success',
+        data: null
+    })
+    // no data is sent...it works
+};
--- backend/routes/expenseRoutes.js
+++ backend/routes/expenseRoutes.js
@@ -3,47 +3,22 @@ const userData = require('../DUMMY_DATA');
 const expenseController = require('../controllers/expenseController');
 const { nanoid } = require('nanoid');
 
-// temporary to get all expense values
-router.get('/', (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        data: userData
-    })
-})
+// middleware to check whether every request has a userId.
+router.use(expenseController.userIdCheck);
+
+//TODO delete, temporary to get all expense values
+router.get('/', expenseController.getAllExpenses);
 
 
 // 'userId', which is the id of the user must be compulsorily passed to all requests
-router.use((req, res, next) => {
-    //check if user exists
-    const foundUser = userData.find(ele => ele._id === req.body.userId);
-    if(!req.body.userId || !foundUser){
-        return res.status(404).json({
-            status: 'error',
-            message: 'Invalid userId'
-        })
-    } else if (!req.body.title || !req.body.amount || !req.body.recurring || !req.body.type || !req.body.date){
-        return res.status(404).json({
-            status: 'error',
-            message: 'Incomplete credentials'
-        })
-    }
+router.post('/add', expenseController.checkFullRequestBody,
+        expenseController.addExpense);
 
-    next();
-})
+// expenseId is passed to every request after this
+router.put('/update', expenseController.checkExpenseId, 
+        expenseController.checkFullRequestBody, expenseController.updateExpense);
 
-
-router.post('/add', (req, res) => {
-    const foundUser = userData.find(ele => ele._id === req.body.userId);
-    const newExpense = Object.assign({_id: nanoid()}, req.body)
-    foundUser.expenses.push(newExpense);
-    res.status(201).json({
-        status: 'success',
-        message: `New expense of ${req.body.title} created.`
-    })
-});
-
-router.put('/update', (req, res) => {});
-
-router.delete('/delete', (req, res) => {});
+router.delete('/delete', expenseController.checkExpenseId,
+        expenseController.deleteExpense);
 
 module.exports = router;
