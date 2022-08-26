a9068827722b21d6d1e86211d7b1756861c56548
Kedar Hargude <hargude481@gmail.com>
2022-08-26 18:17:23 +0530

    Implement Update User info

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -70,11 +70,41 @@ exports.logIn = async (req, res, next) => {
     })
 };
 
-exports.updateUserInfo = (req, res) => {
-    res.status(500).json({
-        status: 'error',
-        message: 'This route is not yet defined! Will update the user info later.'
-      });
+// "userId", name, lastName, mobileNumber, email, password given
+exports.updateUserInfo = async (req, res, next) => {
+
+    const { userId, name, lastName, mobileNumber, email, password } = req.body;
+
+    //check if another same email already exists in database
+    let emailExistingUser;
+    try{
+        emailExistingUser = await User.findOne({email: email});
+    } catch(err){
+        console.log(err);
+        return next(new CustomError(500, 'Fetching user failed. Please try again later.'));
+    }
+    
+    // if to-be-changed-email id's user exists and it's id is not equal to the id of the current user who's requresting the change, show error.
+    if(emailExistingUser && emailExistingUser.id !== userId){
+        return next(new CustomError(400, 'Email already exists, use another email.'))
+    }
+
+    // actually update the user
+    let updatedUser;
+    try{
+        updatedUser = await User.findByIdAndUpdate(userId, {
+            name, lastName, mobileNumber, email, password
+        }, { runValidators: true })
+    } catch(err){
+        console.log(err);
+        return next(new CustomError(500, 'Fetching user failed. Please try again later.'));
+    }
+
+    res.status(200).json({
+        status: 'success',
+        message: 'Updated the user',
+        user: updatedUser
+    });
 }
 
 exports.deleteUser = (req, res) => {
